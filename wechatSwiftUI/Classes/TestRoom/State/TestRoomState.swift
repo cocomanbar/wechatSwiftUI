2f0e2b6feb9d536c61d970483d0dec0bdc0867b4
cocomanbar <125322078@qq.com>
2022-11-19 12:36:43 +0800

    State

--- wechatSwiftUI/Classes/TestRoom/State/TestRoomState.swift
+++ wechatSwiftUI/Classes/TestRoom/State/TestRoomState.swift
@@ -10,9 +10,28 @@ import SwiftUI
 struct TestRoomState: View {
     var title: String = ""
     
+    @State private var  turn: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
-            .navigationTitle(title)
+        
+        ScrollView {
+            LazyVStack(spacing: 20) {
+                Text(turn ? "按钮选中": "按钮还未选中")
+                    .foregroundColor(.primary)
+                    .font(.system(size: 18, weight: .heavy))
+                Text("@State是一个属性包装器，可以用来描述视图的状态。 SwiftUI会将其存储在View struct之外的特殊内存中, 只有相关视图才能访问它。当 @State 装饰过的属性发生了变化，SwiftUI 会根据新的属性值重新创建视图")
+                    .lineLimit(Int.max)
+                    .foregroundColor(.secondary)
+                    .font(.system(size: 16, weight: .regular))
+                Button("点击我") {
+                    self.turn.toggle()
+                }
+                .frame(width: 200, height: 50)
+                .background(.secondary)
+            }
+            .padding(EdgeInsets(top: 50, leading: 20, bottom: 0, trailing: 20))
+        }
+        .navigationTitle(title)
     }
 }
 
